9e8af73920c853bb4bb1e07d10de9ebf07f8d1fb
Bryan Cuneo <bryan.cuneo@gmail.com>
2025-10-03 23:22:06 -0500

    Preparing for 0.0.1 release.

--- test_ps-jsonlogger.ps1
+++ test_ps-jsonlogger.ps1
@@ -18,7 +18,7 @@
 # FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
 # IN THE SOFTWARE.
 
-Import-Module "../src/ps-jsonlogger.psm1" -Force
+Import-Module "./ps-jsonlogger" -Force
 
 class Ctx {
     [string]$Name
